2575f48dd7de72cc9a6434a20a4d59d35e0e72e6
AnantDuhan <duhananant@gmail.com>
2022-01-13 13:00:42 +0530

    Added Firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkZMc2OtHLsECIqoZ4OvbNAYg24f2ZZ3A",
+  authDomain: "e-commerce-8b590.firebaseapp.com",
+  projectId: "e-commerce-8b590",
+  storageBucket: "e-commerce-8b590.appspot.com",
+  messagingSenderId: "1062163676855",
+  appId: "1:1062163676855:web:876ff7619ffe5f928f1732",
+  measurementId: "G-JKYV52FDE6",
+};
